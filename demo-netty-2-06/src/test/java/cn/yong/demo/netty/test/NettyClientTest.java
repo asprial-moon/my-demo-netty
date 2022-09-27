8926b48c7caaf456df87ec327e7a849bfa6ef39d
northline <17317557314@163.com>
2022-09-28 01:07:00 +0800

    netty4.1中级拓展篇六《SpringBoot+Netty+Elasticsearch收集日志信息数据存储》

---
+++ demo-netty-2-06/src/test/java/cn/yong/demo/netty/test/NettyClientTest.java
@@ -0,0 +1,15 @@
+package cn.yong.demo.netty.test;
+
+
+import java.io.File;
+
+/**
+ * @author Line
+ * @desc
+ * @date 2022/9/26
+ */
+public class NettyClientTest {
+    public static void main(String[] args) {
+
+    }
+}
